89c63fb9e0a8a3c44e16b7dd9d8a1ad79dc4f7bc
lazyyq <kykint@naver.com>
2023-06-13 03:01:56 +0900

    Support selecting picture from gallery as well as camera

--- app/src/main/java/com/kykint/fridgeinmyhand/utils/Utils.kt
+++ app/src/main/java/com/kykint/fridgeinmyhand/utils/Utils.kt
@@ -1,7 +1,16 @@
 package com.kykint.fridgeinmyhand.utils
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Looper
+import android.provider.MediaStore
+import android.provider.OpenableColumns
+import android.util.Log
+import androidx.annotation.RequiresApi
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
@@ -59,3 +68,56 @@ fun Bitmap.saveAsFile(outputPath: String): File? {
 
 val isMainThread
     get() = Looper.getMainLooper().isCurrentThread
+
+// https://www.digitalocean.com/community/tutorials/android-capture-image-camera-gallery
+fun Context.getGalleryAndCameraIntents(tempPicUri: Uri): Intent {
+    val intents = arrayListOf<Intent>()
+
+    // Camera intents
+    val captureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    packageManager.queryIntentActivities(captureIntent, 0).map { res ->
+        Intent(captureIntent).apply {
+            component = ComponentName(res.activityInfo.packageName, res.activityInfo.name)
+            setPackage(res.activityInfo.packageName)
+            putExtra(MediaStore.EXTRA_OUTPUT, tempPicUri)
+        }
+    }.let { intents.addAll(it) }
+
+    val galleryIntent = Intent(Intent.ACTION_GET_CONTENT).apply { type = "image/*" }
+    packageManager.queryIntentActivities(galleryIntent, 0).map { res ->
+        Intent(galleryIntent).apply {
+            component = ComponentName(res.activityInfo.packageName, res.activityInfo.name)
+            setPackage(res.activityInfo.packageName)
+        }
+    }.let { intents.addAll(it) }
+
+    return Intent.createChooser(intents.last(), "Select source").apply {
+        putExtra(Intent.EXTRA_INITIAL_INTENTS, intents.toTypedArray())
+    }
+}
+
+// https://stackoverflow.com/a/71309183
+@RequiresApi(Build.VERSION_CODES.O)
+fun Context.createFileFromContentUri(fileUri: Uri, filePath: String): File? {
+    var fileName: String? = null
+
+    fileUri.let { returnUri ->
+        contentResolver.query(returnUri, null, null, null)
+    }?.use { cursor ->
+        val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        cursor.moveToFirst()
+        fileName = cursor.getString(nameIndex)
+    }
+
+    if (fileName == null) {
+        Log.e("createFileFromContentUri()", "Couldn't find file uri")
+        return null
+    }
+
+    val outputFile = File(filePath)
+    contentResolver.openInputStream(fileUri)!!.use { inputStream ->
+        inputStream.copyTo(outputFile.outputStream())
+    }
+
+    return outputFile
+}
